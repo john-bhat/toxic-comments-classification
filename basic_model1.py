f5ade37ad77dc8e23fc229fdd97f24bdab3347be
John Mohd Bhat <jmbhat786@gmail.com>
2017-12-21 14:47:26 +0530

    Update basic_model1.py

--- basic_model1.py
+++ basic_model1.py
@@ -1,12 +1,7 @@
-# This Python 3 environment comes with many helpful analytics libraries installed
-# It is defined by the kaggle/python docker image: https://github.com/kaggle/docker-python
-# For example, here's several helpful packages to load in 
+
 
 import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
-
-# Input data files are available in the "../input/" directory.
-# For example, running this (by clicking run or pressing Shift+Enter) will list the files in the input directory
 
 from subprocess import check_output
 print(check_output(["ls", "../input"]).decode("utf8"))
@@ -16,7 +11,7 @@ print(check_output(["ls", "../input"]).decode("utf8"))
 import numpy as np
 import pandas as pd
 #machine Learning
-from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import MultinomialNB
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
 
